ad1411f3fc48d237f3c4b37154549e02030b6c07
sina sima <simacna@gmail.com>
2020-04-28 16:04:06 -0400

    giddy up

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,8 +4,10 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
   return(
-    <div className={classes.Burger}>
+    <div className={'Burger'}>
+      {/* everything below is hard coded and not being dynamically rendered by state */}
       <BurgerIngredient type="bread-top"/>
+      <BurgerIngredient type="salad"/>
       <BurgerIngredient type="cheese"/>
       <BurgerIngredient type="meat"/>
       <BurgerIngredient type="bread-bottom"/>
