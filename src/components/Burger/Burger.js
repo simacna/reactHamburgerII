5ca441f11d7300288f3626208ab2b230570712a0
sina sima <simacna@gmail.com>
2020-05-01 16:38:30 -0400

    pushing before continuing

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,19 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+  //transfer objct to key/value pairs below
+  const transformedIngredients = Object.keys(props.ingredients)
+  .map(ingKey => {
+    return[...Array()props.ingredients[igKey]].map((_, i)=>{
+      <BurgerIngredient key={igKey+i} type={igKey} />
+    })
+  }); //extracts keys of given object, gives us an array of keys
+
   return(
     <div className={'Burger'}>
       {/* everything below is hard coded and not being dynamically rendered by state */}
       <BurgerIngredient type="bread-top"/>
-      <BurgerIngredient type="salad"/>
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="meat"/>
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom"/>
     </div>
   );
