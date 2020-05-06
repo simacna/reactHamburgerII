88f3f171f80074806f982ef51af60ebd3cea6e2a
sina sima <simacna@gmail.com>
2020-05-06 12:28:17 -0400

    debugging Burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,21 +5,29 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const Burger = (props) => {
   //transfer objct to key/value pairs below
+  console.log('props', props.ingredients);
+  const transformedIngredients = Object.keys(props.ingredients)
+  // .map(ingKey => {
+  //   return([...Array(props.ingredients[ingKey])]).map((_, i)=>{
+  //     // console.log('ingKey', ingKey);
+  //    return (<BurgerIngredient key={ingKey+i} type={ingKey} />)
+  //   });
+  // }); //extracts keys of given object, gives us an array of keys
 
-  let transformedIngredients = Object.keys(props.ingredients)
-  .map(ingKey => {
-    return([...Array(props.ingredients[ingKey])].map((_, i)=>{
-      // console.log('ingKey', ingKey);
-      return(<BurgerIngredient key={ingKey+i} type={ingKey} />)
-    }));
-  }); //extracts keys of given object, gives us an array of keys
+  // let transformedIngridients = Object.keys(props.ingredients)  //converting an object into array
+    .map(ingKey => {
+        return [...Array(props.ingredients[ingKey])] // an array with two elements
+        .map((_, i) => {
+            return (<BurgerIngredient key={ingKey+i} type={ingKey} />)
+        })
+    })
 
   return(
     
     <div className={'Burger'}>
       {/* everything below is hard coded and not being dynamically rendered by state */}
       <BurgerIngredient type="bread-top"/>
-      {transformedIngredients}
+        {transformedIngredients}
       <BurgerIngredient type="bread-bottom"/>
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,7 +23,7 @@ class BurgerBuilder extends Component {
   render(){
 		return(
 				<Aux>
-					<Burger />
+					<Burger ingredients={this.state.ingredients} />
 					<div>Build Controls</div>
 				</Aux>
 
